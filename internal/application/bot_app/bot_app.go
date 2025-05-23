ae5b146b977a119e49f6a495df6c4038b8e94dbc
Andrei Gushchin <neogan74@gmail.com>
2025-05-23 21:35:28 +0300

    feat: development

--- internal/application/bot_app/bot_app.go
+++ internal/application/bot_app/bot_app.go
@@ -1,6 +1,9 @@
-package botapp
+package bot_app
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/neogan74/linker/internal/infra/clients"
+)
 
 type BotApp struct {
 	Bot    *tgbotapi.BotAPI
@@ -12,9 +15,18 @@ func NewBotApp(bot *tgbotapi.BotAPI, client *clients.BotClient) *BotApp {
 		Bot:    bot,
 		Client: client,
 	}
+
 }
 
-func NewBot(b *BotApp) error {
+func NewBot() (*tgbotapi.BotAPI, error) {
+	bot, err := tgbotapi.NewBotAPI("111111111:AAFVYB1Rtibif51ky1lrnnReA5XYQqlqK88")
+	if err != nil {
+		return nil, nil
+	}
+	return bot, nil
+}
+
+func RunBot(b *BotApp) error {
 	go func() {
 		u := tgbotapi.NewUpdate(0)
 		u.Timeout = 60
@@ -27,7 +39,6 @@ func NewBot(b *BotApp) error {
 				if err != nil {
 					msg = tgbotapi.NewMessage(update.Message.Chat.ID, err.Error())
 				}
-
 				_, err = b.Bot.Send(msg)
 				if err == nil {
 					return
--- internal/application/bot_app/bot_commands.go
+++ internal/application/bot_app/bot_commands.go
@@ -1,6 +1,12 @@
-package botapp
+package bot_app
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	"fmt"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/neogan74/linker/internal/errors"
+)
 
 func (b *BotApp) CheckBotCommands(upd tgbotapi.Update, updChan *tgbotapi.UpdatesChannel) (tgbotapi.MessageConfig, error) {
 	switch upd.Message.Command() {
@@ -9,32 +15,83 @@ func (b *BotApp) CheckBotCommands(upd tgbotapi.Update, updChan *tgbotapi.Updates
 	case "help":
 		return b.HelpCommand(upd)
 	case "track":
-		return b.TrackCommand(upd)
+		return b.TrackCommand(upd, updChan)
 	case "untrack":
 		return b.UntrackCommand(upd)
-	case "last":
-		return b.LastCommand(upd)
+	case "list":
+		return b.ListCommand(upd)
 	default:
 		return tgbotapi.MessageConfig{}, errors.UnknownCommand{}
 	}
 }
 
 func (b *BotApp) StartCommand(upd tgbotapi.Update) (tgbotapi.MessageConfig, error) {
-	panic("unimplemented")
+	err := b.Client.SaveTgChat(upd.Message.Chat.ID)
+	if err != nil {
+		return tgbotapi.MessageConfig{}, err
+	}
+	message := tgbotapi.NewMessage(upd.Message.Chat.ID, "Welcome, you are registered!")
+	return message, nil
 }
 
 func (b *BotApp) HelpCommand(upd tgbotapi.Update) (tgbotapi.MessageConfig, error) {
-	panic("unimplemented")
+	message := tgbotapi.NewMessage(upd.Message.Chat.ID, "I can help you with the follwoing commands: /start, /help, /track, /untrack, /list")
+	return message, nil
 }
 
-func (b *BotApp) TrackCommand(upd tgbotapi.Update) (tgbotapi.MessageConfig, error) {
-	panic("unimplemented")
+func (b *BotApp) TrackCommand(upd tgbotapi.Update, updChan *tgbotapi.UpdatesChannel) (tgbotapi.MessageConfig, error) {
+	url := upd.Message.CommandArguments()
+	_, err := b.Bot.Send(tgbotapi.NewMessage(upd.Message.Chat.ID, "Enter tags(space separated)"))
+	if err != nil {
+		return tgbotapi.MessageConfig{}, err
+	}
+	tags := b.CheckUpdates(updChan)
+	_, err = b.Bot.Send(tgbotapi.NewMessage(upd.Message.Chat.ID, "Enter filters(space separated)"))
+	if err != nil {
+		return tgbotapi.MessageConfig{}, err
+	}
+	filters := b.CheckUpdates(updChan)
+	err = b.Client.SaveLink(upd.Message.Chat.ID, url, strings.Split(filters.(string), " "), strings.Split(tags.(string), " "))
+	if err != nil {
+		return tgbotapi.MessageConfig{}, err
+	}
+	message := tgbotapi.NewMessage(upd.Message.Chat.ID, fmt.Sprintf("Now you are following %s", url))
+	return message, nil
 }
 
 func (b *BotApp) UntrackCommand(upd tgbotapi.Update) (tgbotapi.MessageConfig, error) {
-	panic("unimplemented")
+	arg := upd.Message.CommandArguments()
+	err := b.Client.DeleteLink(upd.Message.Chat.ID, arg)
+	if err != nil {
+		return tgbotapi.MessageConfig{}, err
+	}
+
+	message := tgbotapi.NewMessage(upd.Message.Chat.ID, fmt.Sprintf("Now you are not following %s", arg))
+	return message, nil
 }
 
-func (b *BotApp) LastCommand(upd tgbotapi.Update) (tgbotapi.MessageConfig, error) {
-	panic("unimplemented")
+func (b *BotApp) ListCommand(upd tgbotapi.Update) (tgbotapi.MessageConfig, error) {
+	links, err := b.Client.GetLinks(upd.Message.Chat.ID)
+	if err != nil {
+		return tgbotapi.MessageConfig{}, err
+	}
+	if len(links) == 0 {
+		return tgbotapi.NewMessage(upd.Message.Chat.ID, "List of your tracks is empty"), nil
+	}
+
+	t := "There is list of your tracks:\n"
+	for _, link := range links {
+		t += link + "\n"
+	}
+	message := tgbotapi.NewMessage(upd.Message.Chat.ID, t)
+	return message, nil
+}
+
+func (b *BotApp) CheckUpdates(updChan *tgbotapi.UpdatesChannel) any {
+	for upd := range *updChan {
+		if upd.Message != nil {
+			return upd.Message.Text
+		}
+	}
+	return ""
 }
