9eecce9f6174d0089770f1c7fe232b01dc9109aa
Andrei Gushchin <neogan74@gmail.com>
2025-05-24 11:45:04 +0300

    feat: SendNotify func

--- internal/infra/clients/scrapper_client.go
+++ internal/infra/clients/scrapper_client.go
@@ -1,6 +1,13 @@
 package clients
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/neogan74/linker/internal/api/bot_api/codegen"
+	"github.com/neogan74/linker/internal/errors"
+)
 
 type ScapperClinet struct {
 	client *http.Client
@@ -13,5 +20,29 @@ func NewScrapperClient() *ScapperClinet {
 }
 
 func (c *ScapperClinet) SendNotifiy(tgChatID int64, url string) error {
+	req := codegen.LinkUpdate{
+		Description: func() *string {
+			s := "Link Update"
+			return &s
+		}(),
+		Id:        nil,
+		TgChatIds: &[]int64{tgChatID},
+		Url:       &url,
+	}
+	jsonBody, err := json.Marshal(req)
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("POST", "http://localhost:8080/updates", bytes.NewBuffer(jsonBody))
+
+	httpData, err := http.DefaultClient.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	if httpData.StatusCode != http.StatusOK {
+		return errors.BadRequestError{}
+	}
+
 	return nil
 }
